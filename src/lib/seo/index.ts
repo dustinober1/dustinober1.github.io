8462f9ea1025d456e652176c40795fcafb6da5c5
dustinober1 <dober.code@gmail.com>
2026-01-09 20:26:58 -0500

    chore: remove unused files from previous redesigns
    - Remove abandoned ebook progress tracking feature (components, API routes, hooks, migration)
    - Delete unused components: AuthorBioSection, EATSignalsSection, ContactSection
    - Remove unused SEO modules: VoiceSearchOptimizer, AccessibilityManager, MobileOptimizer, ContentOptimizer
    - Delete duplicate whitepaper PDF and outdated planning documents
    - Update seo/index.ts exports
    
    Removes ~24 files totaling ~380KB of dead code.

--- src/lib/seo/index.ts
+++ src/lib/seo/index.ts
@@ -6,17 +6,14 @@ export * from './StructuredDataEngine';
 export * from './SitemapGenerator';
 export * from './EATManager';
 export * from './eatData';
-export * from './VoiceSearchOptimizer';
 
 import { MetadataManager } from './MetadataManager';
 import { StructuredDataEngine } from './StructuredDataEngine';
 import { SitemapGenerator } from './SitemapGenerator';
 import { EATManager } from './EATManager';
-import { VoiceSearchOptimizer } from './VoiceSearchOptimizer';
 import { defaultSEOConfig } from './config';
 
 export const metadataManager = new MetadataManager(defaultSEOConfig);
 export const structuredDataEngine = new StructuredDataEngine();
 export const sitemapGenerator = new SitemapGenerator('https://aiober.com');
 export const eatManager = new EATManager();
-export const voiceSearchOptimizer = new VoiceSearchOptimizer();
